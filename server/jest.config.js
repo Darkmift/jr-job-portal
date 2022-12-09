3af41d973356ff3190330c8828031531c9716075
darkmift <darkmift@gmail.com>
2022-12-09 18:08:43 +0200

    fixed project package definitions,jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,9 +1,24 @@
-module.exports = {
-  testEnvironment: 'node',
-  testEnvironmentOptions: {
-    NODE_ENV: 'test',
-  },
-  restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const rootDirector = path.resolve(path.dirname(fileURLToPath(import.meta.url)));
+
+export default {
+    testEnvironment: 'node',
+    testEnvironmentOptions: {
+        NODE_ENV: 'test',
+    },
+    restoreMocks: true,
+    coveragePathIgnorePatterns: [
+        'node_modules',
+        'src/config',
+        'src/app.js',
+        'tests',
+    ],
+    coverageReporters: ['text', 'lcov', 'clover', 'html'],
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^#(.*)$': `${rootDirector}/src/$1`,
+    },
 };
