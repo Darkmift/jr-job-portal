272cef1124e927ff681ff8b988ce8cc23e07d7ff
darkmift <darkmift@gmail.com>
2023-04-22 22:32:32 +0300

    added initial header components

---
+++ client/src/components/Layout/AppHeader.jsx
@@ -0,0 +1,37 @@
+// scss
+import styles from './AppHeader.module.scss';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import AuthNavPanel from './AuthNavPanel';
+
+// logo
+import Logo from '@/assets/images/svg/kula like.svg';
+import SearchBar from '../common/SeacrhBar';
+
+function AppHeader(props) {
+    return (
+        <header className={styles['app-header']}>
+            <nav>
+                <section>
+                    <img src={Logo} alt="kula like logo" />
+                    <ul>
+                        <li>אודות</li>
+                        <li>משרות עבודה</li>
+                        <li>כתבות</li>
+                        <li>הפורום</li>
+                        <li>צור קשר</li>
+                    </ul>
+                    <SearchBar />
+                </section>
+                <section>
+                    <AuthNavPanel />
+                </section>
+            </nav>
+        </header>
+    );
+}
+
+AppHeader.propTypes = {};
+
+export default AppHeader;
---
+++ client/src/components/Layout/AuthNavPanel/index.jsx
@@ -0,0 +1,24 @@
+// styles
+import styles from './AuthNavPanel.module.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../../common/Button/Button';
+
+function AuthNavPanel(props) {
+    const isLoggedIn = false; //placeholder for logic
+
+    const clickHandler = e => {};
+
+    return isLoggedIn ? (
+        <>TODO add logged in panel</>
+    ) : (
+        <div className={styles['auth-nav-panel']}>
+            <Button onClick={clickHandler} label={'להרשמה'} />
+            <Button onClick={clickHandler} variant="link" label={'להתחברות'} />
+        </div>
+    );
+}
+
+AuthNavPanel.propTypes = {};
+
+export default AuthNavPanel;
