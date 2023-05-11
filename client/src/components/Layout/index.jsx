5a1a8b25247f62801b96e32a1fded6dac1b53207
darkmift <darkmift@gmail.com>
2023-05-11 09:36:16 +0300

    [css-fixes-05/11] fixes

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -6,20 +6,12 @@ import { Outlet } from 'react-router-dom';
 
 function index(props) {
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}
-        >
+        <>
             <AppHeader />
             {/* so small page still fill the remaining screen between heqad and footer */}
-            <div style={{ flex: '1' }}>
-                <Outlet />
-            </div>
+            <Outlet />
             <AppFooter />
-        </div>
+        </>
     );
 }
 
