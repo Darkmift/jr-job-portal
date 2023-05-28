67bda64bcf4687a29e3ee27bacf5b9337131d843
Almog Skalka <almogsk14@gmail.com>
2023-05-28 17:30:11 +0300

    Working on About-page

---
+++ client/src/pages/About/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from '../Home/Welcome.module.scss';
+import WelcomePic from '@/assets/images/svg/welcome-pic.svg';
+import style from './About.module.scss';
+
+export default function index() {
+    return (
+        <>
+            <div className={`${styles.welcomeContainer}`}>
+                <div className={`${styles.welcomeDiv}`}>
+                    <span className={`${styles.title}`}>
+                        מי אנחנו #כולא_לייק?
+                    </span>
+                    <p className={`${styles.text}`}>
+                        קהילת #כולא_לייק הוקמה במאי 2021 במסגרת מיזם השינוי
+                        ההתנדבותי והחינמי #שופר אותו ייסד עדי פורת ובראשה עומדת
+                        רחל פיירמן. כדי להשיג כמחפשי.ות עבודה את המקסימום
+                        מהקהילה שכולה עשייה התנדבותית ונתינה. בקהילה אנחנו
+                        נותנים סיוע בפעילות התנדבותית וכן הכשרה מקצועית ושכלול
+                        מיומנויות.
+                    </p>
+                    <h3 className={styles['secondary-heading']}>
+                        הבמה שלנו...
+                    </h3>
+                    <p className={`${styles.text}`}>
+                        קהילת #כולא_לייק הוקמה במאי 2021 במסגרת מיזם השינוי
+                        ההתנדבותי והחינמי #שופר אותו ייסד עדי פורת ובראשה עומדת
+                        רחל פיירמן. כדי להשיג כמחפשי.ות עבודה את המקסימום
+                        מהקהילה שכולה עשייה התנדבותית ונתינה. בקהילה אנחנו
+                        נותנים סיוע בפעילות התנדבותית וכן הכשרה מקצועית ושכלול
+                        מיומנויות.
+                    </p>
+                    <h3 className={styles['secondary-heading']}>
+                        איך אנחנו יכולים לעזור?
+                    </h3>
+                    <p className={`${styles.text}`}>
+                        קהילת #כולא_לייק הוקמה במאי 2021 במסגרת מיזם השינוי
+                        ההתנדבותי והחינמי #שופר אותו ייסד עדי פורת ובראשה עומדת
+                        רחל פיירמן. כדי להשיג כמחפשי.ות עבודה את המקסימום
+                        מהקהילה שכולה עשייה התנדבותית ונתינה. בקהילה אנחנו
+                        נותנים סיוע בפעילות התנדבותית וכן הכשרה מקצועית ושכלול
+                        מיומנויות.
+                    </p>
+                    <h3 className={styles['secondary-heading']}>
+                        צפו עלינו עוד...
+                    </h3>
+                    <frame className={`${style['community-video']}`}></frame>
+                </div>
+                <div className={`${styles.imgDiv}`}>
+                    <img
+                        className={`${styles.img}`}
+                        src={WelcomePic}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
