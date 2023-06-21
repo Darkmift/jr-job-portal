428ffc7475380f689cd7834645b40db77dabe19c
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2023-06-21 14:05:10 +0300

    edited modal popup style, connected it with sign up button.

--- client/src/components/Layout/AuthNavPanel/index.jsx
+++ client/src/components/Layout/AuthNavPanel/index.jsx
@@ -1,16 +1,20 @@
 // styles
 import styles from './AuthNavPanel.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../common/Button';
+import Modal from '../../common/Modal';
 
 function AuthNavPanel(props) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const isLoggedIn = false; //placeholder for logic
 
-    const clickHandler = e => {};
+    const clickHandler = e => {
+        setIsModalOpen(true);
+    };
 
     return isLoggedIn ? (
-        <>TODO add logged in panel</>
+        <React.Fragment>TODO add logged in panel</React.Fragment>
     ) : (
         <div className={styles['auth-nav-panel']}>
             <Button
@@ -22,6 +26,12 @@ function AuthNavPanel(props) {
                 variant="text"
                 label={'להתחברות'}
             />
+            <Modal
+                isOpen={isModalOpen}
+                setIsOpen={setIsModalOpen}
+            >
+                <div>הרשמה</div>
+            </Modal>
         </div>
     );
 }
--- client/src/components/common/Modal/index.jsx
+++ client/src/components/common/Modal/index.jsx
@@ -1,12 +1,24 @@
 import { Children } from 'react';
 import style from './Modal.module.scss';
+import xIconVector from '../../../assets/images/svg/x-symbol.svg';
 import ReactDom from 'react-dom';
 
-const Modal = ({ children, open }) => {
-    if (!open) return null;
+const Modal = ({ children, isOpen, setIsOpen }) => {
+    if (!isOpen) return null;
     return ReactDom.createPortal(
         <div className={style.modalContainer}>
-            <div className={style.modalPopup}>{children}</div>
+            <div className={style.modalPopup}>
+                <div
+                    className={style.exitButtonDiv}
+                    onClick={() => setIsOpen(false)}
+                >
+                    <img
+                        src={xIconVector}
+                        alt={'exit button'}
+                    />
+                </div>
+                {children}
+            </div>
         </div>,
         document.getElementById('portal')
     );
